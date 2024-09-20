bb17f7f71049c001e95998a1813073cbb6602f85
Santiago Encina <santiago.encina@ing.austral.edu.ar>
2024-09-20 15:30:30 -0300

    remove unused contracts

---
+++ packages/web3/contracts/OEANFTs.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract OEANFTs is ERC721URIStorage {
+    address public owner;
+    mapping (address => uint) owners;
+    uint256 public counter;
+
+    event NFTCreated(uint256 indexed tokenId);
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+        counter = 0;
+    }
+
+    function mint(address to, string memory _tokenURI) public {
+        require(msg.sender == owner, "Only owner can mint");
+        _safeMint(to, counter);
+        _setTokenURI(counter, _tokenURI);
+        owners[to] = counter;
+        emit NFTCreated(counter);
+        counter++;
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(msg.sender == owner, "Only owner can set token URI");
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function authenticate(uint256 tokenId) public view returns (bool) {
+        return owners[msg.sender] == tokenId;
+    }
+}
--- packages/web3/ignition/modules/OEANFTs.ts
+++ packages/web3/ignition/modules/OEANFTs.ts
@@ -1,12 +1,14 @@
 import { ethers } from 'hardhat';
 
 const main = async () => {
-  const MyContract = await ethers.getContractFactory('MyNFT');
+  const MyContract = await ethers.getContractFactory('OEANFTs');
   const metadataHash = 'bafkreiccparztqw5li6osq6ikcvllijutaoaiih6lmthiabnwnkgy66mnq';
-  const myContract = await MyContract.deploy(`ipfs://${metadataHash}`);
+  const myContract = await MyContract.deploy("OEANFTs", "OEA");
   await myContract.waitForDeployment();
   const address = await myContract.getAddress();
   console.log('MyContract deployed to:', address);
+  await myContract.mint('0x9bfcd93Cf0490F82427a4bFD911002438C728D19', metadataHash);
+  console.log('Minted NFT with metadata hash:', metadataHash);
 };
 
 main()
